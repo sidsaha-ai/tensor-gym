5a9f65808ca09b5c96ce1f50b65aaa61f87df234
sidsaha-ai <sid.chilling@gmail.com>
2024-09-27 14:31:14 +0530

    More Exercises (#3)

---
+++ code/16_custom_relu.py
@@ -0,0 +1,63 @@
+"""
+This implements the solution for the exercise https://tensorgym.com/exercises/15
+"""
+import torch
+
+
+class CustomReLUFn(torch.autograd.Function):  # pylint: disable=abstract-method
+    """
+    The custom ReLU function.
+    """
+
+    @staticmethod
+    def forward(ctx, x: torch.Tensor) -> torch.Tensor:  # pylint: disable=arguments-differ
+        """
+        Implements the forward pass.
+        """
+        ctx.save_for_backward(x)
+
+        res = x.masked_fill(x <= 0, 0)
+        return res
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor) -> torch.Tensor:  # pylint: disable=arguments-differ
+        """
+        Implements the backward pass.
+        """
+        inputs, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+
+        dx = inputs.masked_fill(inputs > 0, 1)
+        dx = dx.masked_fill(dx < 1, 0)
+
+        grad_input = grad_output * dx
+
+        return grad_input
+
+
+def compute_custom_relu(inputs: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    """
+    Compute ReLU using the custom ReLU implementation.
+    """
+    relu = CustomReLUFn.apply
+    inputs = inputs.clone().detach().requires_grad_(True)
+    outputs = relu(inputs)
+    outputs.backward(torch.ones_like(inputs))
+    return outputs, inputs.grad
+
+
+def main():
+    """
+    The main function that runs the test cases.
+    """
+    inputs = [-1, -0.5, 1]
+    inputs = torch.tensor(inputs, dtype=torch.float)
+
+    outputs, inputs_grad = compute_custom_relu(inputs)
+
+    print(outputs)
+    print(inputs_grad)
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/17_mse_loss.py
@@ -0,0 +1,69 @@
+"""
+This is the solution to the exercise https://tensorgym.com/exercises/16
+"""
+import torch
+
+
+def mse_loss(inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+    """
+    This function implements the solution to this exercise.
+    """
+    res = torch.sum(torch.pow(inputs - targets, 2)) / inputs.numel()
+    return res
+
+
+def equal(x: torch.Tensor, y: torch.Tensor) -> bool:
+    """
+    Compares the two supplied tensors.
+    """
+    x_str: str = f'{x.item():.4f}'
+    y_str: str = f'{y.item():.4f}'
+    if x_str != y_str:
+        return False
+    return True
+
+
+def main():
+    """
+    This function runs the test cases.
+    """
+    cases = []
+
+    loss_fn = torch.nn.MSELoss()
+
+    # test case 1
+    inputs = [1, 2, 3]
+    targets = [1, 2.5, 2.5]
+    inputs = torch.tensor(inputs, dtype=torch.float)
+    targets = torch.tensor(targets, dtype=torch.float)
+    cases.append({
+        'inputs': inputs,
+        'targets': targets,
+        'outputs': loss_fn(inputs, targets),
+    })
+
+    # test case 2
+    inputs = [
+        [3, -3, 0],
+        [1, 0, 0],
+    ]
+    targets = [
+        [3, -2.5, 0.5],
+        [1, -1, 1],
+    ]
+    inputs = torch.tensor(inputs, dtype=torch.float)
+    targets = torch.tensor(targets, dtype=torch.float)
+    cases.append({
+        'inputs': inputs,
+        'targets': targets,
+        'outputs': loss_fn(inputs, targets),
+    })
+
+    for ix, c in enumerate(cases):
+        res = mse_loss(c.get('inputs'), c.get('targets'))
+        message = f'PASS: test case {ix + 1}' if equal(res, c.get('outputs')) else f'FAIL: test case {ix + 1}'
+        print(message)
+
+
+if __name__ == '__main__':
+    main()
