79e28d1d4a6a9c503eeeab2eb0662988e8d57e4a
Siddharth Saha <siddharth.saha@cred.club>
2024-09-27 18:57:04 +0530

    working with numpy

--- code/18_k_means_clustering.py
+++ code/18_k_means_clustering.py
@@ -9,10 +9,10 @@ def k_means_clustering(data: np.ndarray, k: int) -> np.ndarray:
     """
     The implementation for the solution to this exercise.
     """
-    data = torch.from_numpy(data)
-
+    # init
     rand_ix = torch.randperm(data.shape[0])[0:k]
     centroids = data[rand_ix]
+
     clusters = []
 
     for row in range(data.shape[0]):
@@ -21,15 +21,17 @@ def k_means_clustering(data: np.ndarray, k: int) -> np.ndarray:
         row_data = data[row]
 
         for ix, c in enumerate(centroids):
-            distance = torch.norm(row_data - c).item()
+            distance = torch.norm(
+                torch.tensor(row_data, dtype=torch.float) - torch.tensor(c, dtype=torch.float),
+            ).item()
             target_ix = ix if distance < min_distance else target_ix
             min_distance = min(min_distance, distance)
         
         # assign the row to a cluster
         if len(clusters) < (target_ix + 1):
-            clusters.append([row_data.numpy().tolist()])
+            clusters.append([row_data])
         else:
-            clusters[target_ix].append(row_data.numpy().tolist())
+            clusters[target_ix].append(row_data)
 
     print(clusters)
 
