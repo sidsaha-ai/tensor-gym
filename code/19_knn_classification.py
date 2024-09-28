9b2d62e6b13966655c7b6bdd4d3a49686d3c13c3
Siddharth Saha <siddharth.saha@cred.club>
2024-09-28 17:48:42 +0530

    finished the unoptimized implm of KNN classification

--- code/19_knn_classification.py
+++ code/19_knn_classification.py
@@ -18,9 +18,9 @@ def knn_classifier(
     """
     This function is the implementation of this exercise.
     """
-    predictions = np.zeros(test_data.shape[0])  # the number of test samples to be predicted
+    predictions = np.zeros(test_data.shape[0], dtype=int)  # the number of test samples to be predicted
 
-    for row, tst_data in enumerate(test_data):
+    for row_ix, tst_data in enumerate(test_data):
         distances = np.zeros(train_data.shape[0])  # the distance of this test sample with every train sample
 
         for ix, trn_data in enumerate(train_data):
@@ -28,7 +28,11 @@ def knn_classifier(
 
         sorted_ix = np.argsort(distances)[0:k]  # get the nearest K
         label_freq = Counter(train_labels[ix].item() for ix in sorted_ix)
-        print(label_freq)
+
+        target_label = max(label_freq, key=label_freq.get)
+        predictions[row_ix] = target_label
+    
+    return predictions
 
 def main():
     """
@@ -56,11 +60,34 @@ def main():
         'outputs': np.array(outputs, int),
     })
 
+    # test case 2
+    train_data = [
+        [1, 1.5],
+        [2, 2.5],
+        [3, 3.5],
+    ]
+    train_labels = [0, 1, 0]
+    test_data = [
+        [1.5, 2],
+        [3, 3],
+    ]
+    k: int = 2
+    outputs = [0, 0]
+    cases.append({
+        'train_data': np.array(train_data, dtype=float),
+        'train_labels': np.array(train_labels, dtype=int),
+        'test_data': np.array(test_data, dtype=float),
+        'k': k,
+        'outputs': np.array(outputs, int),
+    })
+
     for ix, c in enumerate(cases):
         res = knn_classifier(
             c.get('train_data'), c.get('train_labels'), c.get('test_data'), c.get('k'),
         )
         print(res)
+        message = f'PASS: test case {ix + 1}' if (res == c.get('outputs')).all() else f'FAIL: test case {ix + 1}'
+        print(message)
 
 
 if __name__ == '__main__':
