17ab404dc2c57df06afb973e9f316379800f5982
Siddharth Saha <siddharth.saha@cred.club>
2024-09-28 18:00:37 +0530

    some vectorization

--- code/19_knn_classification.py
+++ code/19_knn_classification.py
@@ -6,12 +6,6 @@ from collections import Counter
 import numpy as np
 
 
-def euclid_distance(x: np.ndarray, y: np.ndarray) -> float:
-    """
-    Finds the distance between x and y.
-    """
-    return np.sqrt(np.sum(np.pow(x - y, 2)).item())
-
 def knn_classifier(
         train_data: np.ndarray, train_labels: np.ndarray, test_data: np.ndarray, k: int,
 ) -> np.ndarray:
@@ -21,10 +15,9 @@ def knn_classifier(
     predictions = np.zeros(test_data.shape[0], dtype=int)  # the number of test samples to be predicted
 
     for row_ix, tst_data in enumerate(test_data):
-        distances = np.zeros(train_data.shape[0])  # the distance of this test sample with every train sample
-
-        for ix, trn_data in enumerate(train_data):
-            distances[ix] = euclid_distance(tst_data, trn_data)
+        distances = np.sqrt(np.sum(
+            np.pow(tst_data - train_data, 2), axis=1,
+        ))
 
         sorted_ix = np.argsort(distances)[0:k]  # get the nearest K
         label_freq = Counter(train_labels[ix].item() for ix in sorted_ix)
