c32504955f9c26a633845614f36da127d687c000
Siddharth Saha <siddharth.saha@cred.club>
2024-09-28 17:29:40 +0530

    some more progress in KNN classifier

--- code/19_knn_classification.py
+++ code/19_knn_classification.py
@@ -3,6 +3,14 @@ This contains the solution to the exercise https://tensorgym.com/exercises/11.
 """
 import numpy as np
 
+from collections import Counter
+
+def euclid_distance(x: np.ndarray, y: np.ndarray) -> float:
+    """
+    Finds the distance between x and y.
+    """
+    return np.sqrt(np.sum(np.pow(x - y, 2)).item())
+
 def knn_classifier(
         train_data: np.ndarray, train_labels: np.ndarray, test_data: np.ndarray, k: int,
 ) -> np.ndarray:
@@ -11,10 +19,15 @@ def knn_classifier(
     """
     predictions = np.zeros(test_data.shape[0])  # the number of test samples to be predicted
 
-    for row, tdata in enumerate(test_data):
-        print(row)
-        print(tdata)
+    for row, tst_data in enumerate(test_data):
+        distances = np.zeros(train_data.shape[0])  # the distance of this test sample with every train sample
 
+        for ix, trn_data in enumerate(train_data):
+            distances[ix] = euclid_distance(tst_data, trn_data)
+
+        sorted_ix = np.argsort(distances)[0:k]  # get the nearest K
+        label_freq = Counter(train_labels[ix].item() for ix in sorted_ix)
+        print(label_freq)
 
 def main():
     """
